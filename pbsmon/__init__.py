92d74c962337391acfe7ad268f5d735d5b216b3e
Tomer Diament <tomerdiament@power8.tau.ac.il>
2017-06-27 13:23:04 +0300

    checknfs command

--- pbsmon/__init__.py
+++ pbsmon/__init__.py
@@ -3,6 +3,7 @@ from node import getnodes, getclusters
 from queue import getqueues
 from job import getjobs
 from size import Size
+from checknfs import checknfs
 import runtime
 import monitor
 import web
